092aef12404fc917acd6c734a77b3047063d1896
hhiyul <rkdrnr9945@gmail.com>
2025-11-19 11:19:57 +0900

    ㅇㅇ
    ㅇㅇ

--- kt_backend/src/main/kotlin/org/example/kt_backend/service/dto/InferenceResponse.kt
+++
@@ -1,8 +0,0 @@
-package org.example.kt_backend.service.dto
-
-data class InferenceResponse(
-    val filename: String,
-    val content_type: String?,
-    val size_bytes: Int,
-    val prediction: String
-)
